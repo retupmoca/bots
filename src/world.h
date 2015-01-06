505c45dcf36537f0aa466e36ca4f1aaca83e66bf
Andrew Egeler <andrew@egeler.us>
2015-01-05 22:22:43 -0500

    Start adding DLL stuff

--- src/world.h
+++ src/world.h
@@ -2,7 +2,14 @@
 #define WORLD_H
 #include "struct.h"
 
-void world_tick(world*);
-void world_add_bot(world*, machine*, bot_physics*);
+#define DLL_EXPORT
+#ifdef _WIN32
+#ifdef BUILD_SHARED
+#define DLL_EXPORT __declspec(dllexport)
+#endif
+#endif
+
+DLL_EXPORT void world_tick(world*);
+DLL_EXPORT void world_add_bot(world*, machine*, bot_physics*);
 
 #endif
