215cfa29d418cfb664ad5841d01ce79e58460451
wentam <wentam42@gmail.com>
2015-05-31 22:31:35 -0400

    bot shooting and bullet speed tweak

--- src/world.c
+++ src/world.c
@@ -5,152 +5,151 @@
 #include <bots/world.h>
 #include <bots/cpu.h>
 
-void bots_world_tick(bots_world* w){
-    int i = 0;
-    bots_shot* s = 0;
-    /* physics */
-    /* shots */
-    for(s=w->shots[0], i=0; s; s=w->shots[++i]) {
-        /* check collision */
-        int hit = 0;
-        int j;
-        for(j=0; j < w->num_tanks; j++){
-            if(   s->x >= w->tanks[j]->x - 5
-               && s->x <= w->tanks[j]->x + 5
-               && s->y >= w->tanks[j]->y - 5
-               && s->y <= w->tanks[j]->y + 5){
-                /* hit! */
-                hit = 1;
-                
-                /* record damage */
-                w->tanks[j]->health -= 5;
-                if(w->tanks[j]->health < 0)
-                    w->tanks[j]->health = 0;
-                
-                /* delete the shot */
-                free(s);
-                int k;
-                for(k=i; w->shots[k]; k++){
-                    w->shots[k] = w->shots[k+1];
-                }
-                i--;
-                break;
-            }
-        }
-        if(hit)
-            continue;
-        
-        /* move the shots */
-        double rangle = (s->heading-64) * M_PI / 128;
-        int dist = 5;
-        int dx = floor(0.5 + (dist * cos(rangle)));
-        int dy = floor(0.5 + (dist * sin(rangle)));
-        s->x += dx;
-        s->y += dy;
-        
-        /* check collision again */
-        for(j=0; j < w->num_tanks; j++){
-            if(   s->x >= w->tanks[j]->x - 5
-               && s->x <= w->tanks[j]->x + 5
-               && s->y >= w->tanks[j]->y - 5
-               && s->y <= w->tanks[j]->y + 5){
-                /* hit! */
-                
-                /* record damage */
-                w->tanks[j]->health -= 5;
-                if(w->tanks[j]->health < 0)
-                    w->tanks[j]->health = 0;
-                
-                /* delete the shot */
-                free(s);
-                int k;
-                for(k=i; w->shots[k]; k++){
-                    w->shots[k] = w->shots[k+1];
-                }
-                i--;
-            }
-        }
-    }
-    /* bots */
-    for(i=0; i < w->num_tanks; i++){
-        if(w->tanks[i]->health == 0)
-            continue;
-        /* turn, etc */
-        uint16_t throttle;
-        uint16_t steering;
-        uint16_t turret_steering;
-        uint16_t scanner_steering;
-        
-        throttle = w->cpus[i]->ports[0] << 8;
-        throttle = throttle | w->cpus[i]->ports[1];
-        
-        steering = w->cpus[i]->ports[2] << 8;
-        steering = steering | w->cpus[i]->ports[3];
-        steering = steering % 256;
-        
-        w->tanks[i]->heading = (w->tanks[i]->heading + steering) % 256;
-        w->cpus[i]->ports[2] = 0;
-        w->cpus[i]->ports[3] = 0;
-        w->tanks[i]->speed = throttle;
+void bots_world_tick(bots_world *w) {
+  int i = 0;
+  bots_shot *s = 0;
+  /* physics */
+  /* shots */
+  for (s = w->shots[0], i = 0; s; s = w->shots[++i]) {
+    /* check collision */
+    int hit = 0;
+    int j;
+    for (j = 0; j < w->num_tanks; j++) {
+      if (s->x >= w->tanks[j]->x - 5 && s->x <= w->tanks[j]->x + 5 &&
+          s->y >= w->tanks[j]->y - 5 && s->y <= w->tanks[j]->y + 5) {
+        /* hit! */
+        hit = 1;
 
-        /* drive! */
-        double rangle = (w->tanks[i]->heading-64) * M_PI / 128;
-        int dist = w->tanks[i]->speed;
-        int dx = floor(0.5 + (dist * cos(rangle)));
-        int dy = floor(0.5 + (dist * sin(rangle)));
-        w->tanks[i]->x += dx;
-        w->tanks[i]->y += dy;
-        
-        /* turn turret */
-        turret_steering = w->cpus[i]->ports[4] << 8;
-        turret_steering = turret_steering | w->cpus[i]->ports[5];
-        if(w->cpus[i]->ports[7]) /* turret keepshift */
-            turret_steering = (turret_steering + 256 - steering) % 256;
-        
-        w->tanks[i]->turret_offset = (w->tanks[i]->turret_offset + turret_steering) % 256;
-        w->cpus[i]->ports[4] = 0;
-        w->cpus[i]->ports[5] = 0;
-        
-        /* turn scanner */
-        scanner_steering = w->cpus[i]->ports[8] << 8;
-        scanner_steering = scanner_steering | w->cpus[i]->ports[9];
-        
-        if(w->cpus[i]->ports[0x0b]) /* scanner hull keepshift */
-            scanner_steering = (scanner_steering + 256 - steering) % 256;
-        
-        if(w->cpus[i]->ports[0x0d]) /* scanner gun keepshift */
-            scanner_steering = (scanner_steering + 256 - turret_steering) % 256;
-        
-        /* if we're keepshifting on the gun, but not the hull, we need to undo the
-         * turret steering caused by its own keepshift
-         */
-        if(w->cpus[i]->ports[0x0d] && w->cpus[i]->ports[7] && !w->cpus[i]->ports[0x0b])
-            scanner_steering = (scanner_steering + steering) % 256;
-        
-        w->tanks[i]->scanner_offset = (w->tanks[i]->scanner_offset + scanner_steering) % 256;
-        w->cpus[i]->ports[8] = 0;
-        w->cpus[i]->ports[9] = 0;
-    }
+        /* record damage */
+        w->tanks[j]->health -= 5;
+        if (w->tanks[j]->health < 0)
+          w->tanks[j]->health = 0;
 
-    /* execute */
-    for(i=0; i < w->num_tanks; i++){
-        if(w->tanks[i]->health == 0)
-            continue;
-        /* we do these "backwords" in order to simulate a 3-stage pipeline */
-        bots_cpu_execute(w->cpus[i]);
-        bots_cpu_decode(w->cpus[i]);
-        bots_cpu_fetch(w->cpus[i]);
+        /* delete the shot */
+        free(s);
+        int k;
+        for (k = i; w->shots[k]; k++) {
+          w->shots[k] = w->shots[k + 1];
+        }
+        i--;
+        break;
+      }
     }
+    if (hit)
+      continue;
+
+    /* move the shots */
+    double rangle = (s->heading - 64) * M_PI / 128;
+    int dist = 20;
+    int dx = floor(0.5 + (dist * cos(rangle)));
+    int dy = floor(0.5 + (dist * sin(rangle)));
+    s->x += dx;
+    s->y += dy;
+
+    /* check collision again */
+    for (j = 0; j < w->num_tanks; j++) {
+      if (s->x >= w->tanks[j]->x - 5 && s->x <= w->tanks[j]->x + 5 &&
+          s->y >= w->tanks[j]->y - 5 && s->y <= w->tanks[j]->y + 5) {
+        /* hit! */
+
+        /* record damage */
+        w->tanks[j]->health -= 5;
+        if (w->tanks[j]->health < 0)
+          w->tanks[j]->health = 0;
+
+        /* delete the shot */
+        free(s);
+        int k;
+        for (k = i; w->shots[k]; k++) {
+          w->shots[k] = w->shots[k + 1];
+        }
+        i--;
+      }
+    }
+  }
+  /* bots */
+  for (i = 0; i < w->num_tanks; i++) {
+    if (w->tanks[i]->health == 0)
+      continue;
+    /* turn, etc */
+    uint16_t throttle;
+    uint16_t steering;
+    uint16_t turret_steering;
+    uint16_t scanner_steering;
+
+    throttle = w->cpus[i]->ports[0] << 8;
+    throttle = throttle | w->cpus[i]->ports[1];
+
+    steering = w->cpus[i]->ports[2] << 8;
+    steering = steering | w->cpus[i]->ports[3];
+    steering = steering % 256;
+
+    w->tanks[i]->heading = (w->tanks[i]->heading + steering) % 256;
+    w->cpus[i]->ports[2] = 0;
+    w->cpus[i]->ports[3] = 0;
+    w->tanks[i]->speed = throttle;
+
+    /* drive! */
+    double rangle = (w->tanks[i]->heading - 64) * M_PI / 128;
+    int dist = w->tanks[i]->speed;
+    int dx = floor(0.5 + (dist * cos(rangle)));
+    int dy = floor(0.5 + (dist * sin(rangle)));
+    w->tanks[i]->x += dx;
+    w->tanks[i]->y += dy;
+
+    /* turn turret */
+    turret_steering = w->cpus[i]->ports[4] << 8;
+    turret_steering = turret_steering | w->cpus[i]->ports[5];
+    if (w->cpus[i]->ports[7]) /* turret keepshift */
+      turret_steering = (turret_steering + 256 - steering) % 256;
+
+    w->tanks[i]->turret_offset =
+        (w->tanks[i]->turret_offset + turret_steering) % 256;
+    w->cpus[i]->ports[4] = 0;
+    w->cpus[i]->ports[5] = 0;
+
+    /* turn scanner */
+    scanner_steering = w->cpus[i]->ports[8] << 8;
+    scanner_steering = scanner_steering | w->cpus[i]->ports[9];
+
+    if (w->cpus[i]->ports[0x0b]) /* scanner hull keepshift */
+      scanner_steering = (scanner_steering + 256 - steering) % 256;
+
+    if (w->cpus[i]->ports[0x0d]) /* scanner gun keepshift */
+      scanner_steering = (scanner_steering + 256 - turret_steering) % 256;
+
+    /* if we're keepshifting on the gun, but not the hull, we need to undo the
+     * turret steering caused by its own keepshift
+     */
+    if (w->cpus[i]->ports[0x0d] && w->cpus[i]->ports[7] &&
+        !w->cpus[i]->ports[0x0b])
+      scanner_steering = (scanner_steering + steering) % 256;
+
+    w->tanks[i]->scanner_offset =
+        (w->tanks[i]->scanner_offset + scanner_steering) % 256;
+    w->cpus[i]->ports[8] = 0;
+    w->cpus[i]->ports[9] = 0;
+  }
+
+  /* execute */
+  for (i = 0; i < w->num_tanks; i++) {
+    if (w->tanks[i]->health == 0)
+      continue;
+    /* we do these "backwords" in order to simulate a 3-stage pipeline */
+    bots_cpu_execute(w->cpus[i]);
+    bots_cpu_decode(w->cpus[i]);
+    bots_cpu_fetch(w->cpus[i]);
+  }
 }
 
-void bots_world_add_bot(bots_world* w, bots_cpu* m, bots_tank* p) {
-    if(w->num_tanks == 16){
-        return;
-    }
-    m->world = w;
-    m->bot_id = w->num_tanks;
-    m->ports[0x17] = m->bot_id;
-    w->cpus[w->num_tanks] = m;
-    w->tanks[w->num_tanks] = p;
-    w->num_tanks++;
+void bots_world_add_bot(bots_world *w, bots_cpu *m, bots_tank *p) {
+  if (w->num_tanks == 16) {
+    return;
+  }
+  m->world = w;
+  m->bot_id = w->num_tanks;
+  m->ports[0x17] = m->bot_id;
+  w->cpus[w->num_tanks] = m;
+  w->tanks[w->num_tanks] = p;
+  w->num_tanks++;
 }
