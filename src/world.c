60d42576b07d6fab79c4868b706bdb1e7cef34f6
Andrew Egeler <andrew@egeler.us>
2015-06-02 22:57:51 -0400

    attempt #26 (radar keepshift not working)

--- src/world.c
+++ src/world.c
@@ -80,6 +80,7 @@ void bots_world_tick(bots_world* w){
         /* turn, etc */
         uint16_t throttle;
         short real_steering;
+        short real_turret_steering;
         uint16_t steering;
         uint16_t turret_steering;
         uint16_t scanner_steering;
@@ -122,40 +123,40 @@ void bots_world_tick(bots_world* w){
         turret_steering = w->cpus[i]->ports[4] << 8;
         turret_steering = turret_steering | w->cpus[i]->ports[5];
         if(w->cpus[i]->ports[7]) /* turret keepshift */
-            turret_steering = (turret_steering + 256 - steering) % 256;
+            turret_steering = (turret_steering + 256 - real_steering) % 256;
         turret_steering = turret_steering % 256;
-        real_steering = turret_steering;
-        if(real_steering <= 128 && real_steering > 2){
-            real_steering = 2;
+        real_turret_steering = turret_steering;
+        if(real_turret_steering <= 128 && real_turret_steering > 2){
+            real_turret_steering = 2;
         }
-        if(real_steering > 128 && real_steering < 254){
-            real_steering = 254;
+        if(real_turret_steering > 128 && real_turret_steering < 254){
+            real_turret_steering = 254;
         }
-        turret_steering -= real_steering;
-        if(real_steering < 128)
-            real_steering = 0 - real_steering;
+        turret_steering -= real_turret_steering;
+        if(real_turret_steering < 128)
+            real_turret_steering = 0 - real_turret_steering;
         else
-            real_steering = 0 - real_steering - 256;
+            real_turret_steering = 0 - real_turret_steering - 256;
         w->cpus[i]->ports[4] = turret_steering >> 8;
         w->cpus[i]->ports[5] = turret_steering & 0xff;
 
-        w->tanks[i]->turret_offset = (w->tanks[i]->turret_offset + real_steering) % 256;
+        w->tanks[i]->turret_offset = (w->tanks[i]->turret_offset + real_turret_steering) % 256;
 
         /* turn scanner */
         scanner_steering = w->cpus[i]->ports[8] << 8;
         scanner_steering = scanner_steering | w->cpus[i]->ports[9];
 
         if(w->cpus[i]->ports[0x0b]) /* scanner hull keepshift */
-            scanner_steering = (scanner_steering + 256 - steering) % 256;
+            scanner_steering = (scanner_steering + 256 - real_steering) % 256;
 
         if(w->cpus[i]->ports[0x0d]) /* scanner gun keepshift */
-            scanner_steering = (scanner_steering + 256 - turret_steering) % 256;
+            scanner_steering = (scanner_steering + 256 - real_turret_steering) % 256;
 
         /* if we're keepshifting on the gun, but not the hull, we need to undo the
          * turret steering caused by its own keepshift
          */
         if(w->cpus[i]->ports[0x0d] && w->cpus[i]->ports[7] && !w->cpus[i]->ports[0x0b])
-            scanner_steering = (scanner_steering + steering) % 256;
+            scanner_steering = (scanner_steering + real_steering) % 256;
 
         w->tanks[i]->scanner_offset = (w->tanks[i]->scanner_offset + scanner_steering) % 256;
         w->cpus[i]->ports[8] = 0;
