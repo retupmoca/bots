e95112ff7698d919a4f5c4ebc559406aaa98cd47
Andrew <andrew@egeler.us>
2020-01-20 20:10:24 -0500

    iostream

--- GNUmakefile
+++ GNUmakefile
@@ -17,7 +17,7 @@ lib : lib/libbots.so
 **/*.o : include/**/*.h
 
 bin/bots : cli_src/main.o lib/libbots.so
-	$(CXX) $(CXXFLAGS) -o bin/bots -Llib cli_src/main.o -lbots
+	$(CXX) $(CXXFLAGS) -o bin/bots -Llib cli_src/main.o -lbots -lfmt
 
 bin/bots_asm : asm_src/main.o lib/libbots.so
 	$(CC) -g -o bin/bots_asm -Llib asm_src/main.o -lbots
--- cli_src/main.cpp
+++ cli_src/main.cpp
@@ -1,10 +1,9 @@
-#include <cstdio>
+#include <iostream>
+#include <fmt/format.h>
 #include <chrono>
 #include <thread>
 
 #include <bots/bots.h>
-
-using std::printf, std::this_thread::sleep_for, std::chrono::milliseconds;
 
 int main(int argc, char *argv[]) {
     bots_world *g = bots_create_world();
@@ -12,12 +11,12 @@ int main(int argc, char *argv[]) {
     if(argc == 3) {
         if(   !bots_add_bot_from_file(g, argv[1])
            || !bots_add_bot_from_file(g, argv[2])) {
-            printf("Unable to load bots\n");
+            std::cout << "Unable to load bots\n";
             return 1;
         }
     }
     else {
-        printf("Please provide two robots\n");
+        std::cout << "Please provide two robots\n";
     }
 
     bots_spawn(g);
@@ -26,10 +25,11 @@ int main(int argc, char *argv[]) {
 
     while(1) {
         bots_tick(g);
-        printf("Bot 1 heading: %i\n", g->tanks[0]->heading);
+        std::cout << fmt::format("Bot 1 heading: {}\n", g->tanks[0]->heading);
         printf("Bot 1 position: %i:%i\n", g->tanks[0]->x, g->tanks[0]->y);
-        printf("Bot 2 position: %i:%i\n", g->tanks[1]->x, g->tanks[1]->y);
-        sleep_for(milliseconds(1000));
+        std::cout << fmt::format("Bot 1 position: {}:{}\n", g->tanks[0]->x, g->tanks[0]->y);
+        std::cout << fmt::format("Bot 2 position: {}:{}\n", g->tanks[1]->x, g->tanks[1]->y);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
     bots_free_world(g);
     return 0;
